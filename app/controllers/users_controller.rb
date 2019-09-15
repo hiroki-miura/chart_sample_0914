4808cc53c3c81241b380b84314f4d9623906c097
hiroki-miura <mrbigrabu0221@yahoo.co.jp>
2019-09-15 09:57:15 +0900

    add include_gon => application.html.erb & fin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
     gon.labels = ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"]
-    gon.scores = [12,19,3,5,2,3]
+    gon.scores = [1,2,3,4,5,6]
   end
 end
